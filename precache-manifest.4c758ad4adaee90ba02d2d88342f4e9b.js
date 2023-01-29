a0e82eab823ec5ef3fcaad8b0f465495f8b6d66e
prikshit-singh <prikshitlatherlather@gmail.com>
2023-01-29 14:19:24 +0530

    Updates

---
+++ precache-manifest.4c758ad4adaee90ba02d2d88342f4e9b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8caeb1e56444dd805be",
+    "url": "/youthindia/static/css/main.3fa069e1.chunk.css"
+  },
+  {
+    "revision": "d8caeb1e56444dd805be",
+    "url": "/youthindia/static/js/main.d8caeb1e.chunk.js"
+  },
+  {
+    "revision": "0c282120c6a082b63aec",
+    "url": "/youthindia/static/js/1.0c282120.chunk.js"
+  },
+  {
+    "revision": "9d7944bf866b8b1ee438",
+    "url": "/youthindia/static/js/2.9d7944bf.chunk.js"
+  },
+  {
+    "revision": "654df2e80c9860560558",
+    "url": "/youthindia/static/js/3.654df2e8.chunk.js"
+  },
+  {
+    "revision": "69a2c0dac63a835adb60",
+    "url": "/youthindia/static/css/4.94ea1734.chunk.css"
+  },
+  {
+    "revision": "69a2c0dac63a835adb60",
+    "url": "/youthindia/static/js/4.69a2c0da.chunk.js"
+  },
+  {
+    "revision": "f8c988854b9f2ff65d73",
+    "url": "/youthindia/static/js/5.f8c98885.chunk.js"
+  },
+  {
+    "revision": "08c6df2cf2bdef2e0055",
+    "url": "/youthindia/static/js/runtime~main.08c6df2c.js"
+  },
+  {
+    "revision": "6d2103d1bff86c60befa03095f8c6418",
+    "url": "/youthindia/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/youthindia/precache-manifest.4c758ad4adaee90ba02d2d88342f4e9b.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/youthindia/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
